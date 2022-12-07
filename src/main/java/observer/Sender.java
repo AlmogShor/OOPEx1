1c5a66d6dc9c258e51478e265052db34b5c5aeff
Almog <shoralmog@gmail.com>
2022-12-07 11:10:39 +0200

    Comments from Ex0

--- src/main/java/observer/Sender.java
+++ src/main/java/observer/Sender.java
@@ -1,10 +1,10 @@
 package observer;
 
 public interface Sender {
-    //methods to register and unregister observers
+    //methods to register and unregister observers //Just like Add in Ex0
     void register(Member obj);
     void unregister(Member obj);
-    //Inserts the string into this character sequence.
+    //Inserts the string into this character sequence. //Just like remove in Ex0
     void insert(int offset, String obj);
     // Appends the specified string to this character sequence.
     void append(String obj);
